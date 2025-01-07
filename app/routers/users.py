98108a17fe938645672c8f1cfa7c3208d79fe60a
KangIn <gangin0218@gmail.com>
2025-01-07 17:44:15 +0900

    feat:사용자 hard, soft delete 구현완료

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database.session import get_db
-from app.services.user import get_all_users
+from app.services.user_service import get_all_users
+from app.models.user import User
+from app.services.user_service import user_soft_delete, user_hard_delete
 
 router = APIRouter(
     prefix="/user",
@@ -15,3 +17,28 @@ def read_users(db: Session = Depends(get_db)):
     """
     users = get_all_users(db)
     return {"users": users}
+
+
+@router.delete("/user/soft/{user_id}")
+def delete_user(user_id : int, db: Session = Depends(get_db)):
+    user = db.get(User, user_id)  # user_id에 대한 유저 조회
+
+    # 유저가 없을 경우 예외 처리
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    user_soft_delete(user, db)
+
+    return {"message": f"User {user_id} is soft deleted"}
+
+@router.delete("/user/hard/{user_id}")
+def delete_user(user_id : int, db: Session = Depends(get_db)):
+    user = db.get(User, user_id)  # user_id에 대한 유저 조회
+
+    # 유저가 없을 경우 예외 처리
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    user_hard_delete(user, db)
+
+    return {"message": f"User {user_id} is hard deleted"}
---
+++ app/services/user_service.py
@@ -0,0 +1,22 @@
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
+from app.models.user import User
+from fastapi import HTTPException
+def get_all_users(db: Session):
+    return db.query(User).all()
+
+
+def user_soft_delete(user: User, db: Session):
+    user.is_deleted = True  # is_deleted 필드를 True로... (soft delete)
+    db.commit()  # 변경 사항 커밋
+    db.refresh(user)  # 세션에서 객체 새로고침
+
+def user_hard_delete(user: User, db: Session):
+    try:
+        # 테이블에서 사용자 삭제
+        db.rollback()  # 트랜잭션 롤백
+        db.delete(user)
+        db.commit()  # 변경사항 커밋
+        db.refresh(user)  # 세션에서 객체 새로고침
+    except IntegrityError as e:
+        raise HTTPException(status_code=500, detail="Failed to delete user due to integrity constraints")
