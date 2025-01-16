4b709e4cb1ab3f173a5d075578c08f472475e4de
DHKim7762 <149556729+ehdgusdl@users.noreply.github.com>
2025-01-16 20:52:45 +0900

    feat: 평가점수 평균조회 API 구현

--- app/routers/feedback.py
+++ app/routers/feedback.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter, Depends, UploadFile, HTTPException
 from sqlalchemy.orm import Session
 from app.database.session import get_db
 from app.config.azure.pronunciation_feedback import analyze_pronunciation_with_azure
-from app.services.feedback_service import get_value
+from app.schemas.ResultResponseModel import ResultResponseModel
+from app.services.feedback_service import get_value, get_avg_score
 from app.models.sentence import Sentence
 from app.config.openAI.openai_service import get_pronunciation_feedback
 from app.models.feedback import Feedback
@@ -71,3 +72,8 @@ async def analyze_pronunciation_endpoint(
     except Exception as e:
         print(f"[ERROR] {e}")
         raise HTTPException(status_code=500, detail=f"발음 평가 중 오류가 발생했습니다: {str(e)}")
+
+@router.get("/{user_id}/{sentence_id}/score", summary="최근 발음 평가 결과 평균 점수 조회", description="특정 사용자의 최근 발음 평가 평균 점수 결과 조회")
+def get_user_avg_score(user_id: int, db: Session = Depends(get_db)):
+    feedbacks_score = get_avg_score(user_id, db)
+    return ResultResponseModel(code=200, message="상황 문장 목록 조회 성공", data=feedbacks_score)
